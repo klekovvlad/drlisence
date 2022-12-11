e80748422fc384ed26c65891a8038ddca6f3fe5c
klekovvlad <100075182+klekovvlad@users.noreply.github.com>
2022-12-11 20:54:42 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -74,3 +74,15 @@ window.addEventListener('scroll', () => {
     }
 
 });
+
+//Анимация печати
+let typed = document.querySelector('.typing');
+if(typed) {
+    let typed = new Typed('.typing', { // Тут id того блока, в которм будет анимация
+        stringsElement: '#typed-strings', // Тут id блока из которого берем строки для анимации
+        typeSpeed: 100, // Скорость печати
+        startDelay: 500, // Задержка перед стартом анимации
+        backSpeed: 50, // Скорость удаления
+        loop: true // Указываем, повторять ли анимацию
+    });
+}
